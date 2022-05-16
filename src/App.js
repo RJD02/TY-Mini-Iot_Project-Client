d18d0a69b7449d2186a69de6dc3a6e510d3e8ee3
RJD02 <dulangeraviraj@gmail.com>
2022-05-16 09:29:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Pets from "./pages/Pets/Pets";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/pets" element={<Pets />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
